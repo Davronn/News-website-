6eaad0ee48c8440727e61cb598f657f1747afac6
Davronn <davronmahamadjonov8@gmai.com>
2024-03-15 14:34:52 +0500

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "../scss/header.scss";
 function Header() {
   return (
     <>
-      <Link to="/">
-        <p>Home</p>
-      </Link>
-      <Link to="/detelis">
-        <p>Detelis</p>
-      </Link>
+      <header>
+        <nav className="container">
+          <ul>
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="/">
+              <li>Business</li>
+            </Link>
+            <Link to="/">
+              <li>Entertainment</li>
+            </Link>
+            <Link to="/">
+              <li>General</li>
+            </Link>
+            <Link to="/">
+              <li>Health</li>
+            </Link>
+            <Link to="/">
+              <li>Science</li>
+            </Link>
+            <Link to="/">
+              <li>Sports</li>
+            </Link>
+            <Link to="/">
+              <li>Technology</li>
+            </Link>
+          </ul>
+        </nav>
+      </header>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "../scss/home.scss";
 
 function Home() {
+  const [post, setpost] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/posts");
+        const data = await res.json();
+        setpost(data);
+      } catch (error) {
+        console.log(error.massege);
+      }
+    };
+    fetchPosts();
+  }, []);
   return (
-    <h1>Home</h1>
-  )
+    <>
+      <div className="home_page container">
+        <h1>Our Latest Posts</h1>
+        <div className="cards">
+          <div className="card">
+            {post.length > 0 && (post.map((post) => (
+              <li>{post.title}</li>,
+              <div>{post.url}</div>
+            )))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
